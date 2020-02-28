df2c97596abaffeadcea1c6ecac52dab002e4b05
Lucia <lucia.iosa@outlook.com>
2020-02-28 15:07:31 +0100

    Added styles and created new component ResultDetail

--- restaurant_app/src/components/ResultsList.js
+++ restaurant_app/src/components/ResultsList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 // FlatList es un componente de react native que se utiliza cuando se necesita scrollable list de datos
+import ResultDetail from './ResultDetail';
 
 const ResultsList = ({title, results}) => {
     console.log(results);
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList
                 horizontal   // es lo mismo que horizontal={true}
+                showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={(result) => result.id}
                 renderItem={({item}) => {  // item es un objeto que contiene todos los datos de results
-                return <Text>{item.name}</Text>
+                return <ResultDetail result={item}/>
                 }}
             />
         </View>
@@ -20,9 +22,14 @@ const ResultsList = ({title, results}) => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10
+    },
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
     }
 })
 
--- restaurant_app/src/screens/SearchScreen.js
+++ restaurant_app/src/screens/SearchScreen.js
@@ -2,7 +2,9 @@
 // useEffect = hook (function) that allows us to execute some code ONLY the first time that our component renders to the screen
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+// ScrollView detecta que tiene demasiado contenido dentro para que quepa en la
+// pantalla al mismo tiempo, y habilitará automáticamente el scroll (desplazamiento).
 import SearchBar from '../components/SearchBar';
 // importar el hook creado por nosotros. Se ha sacado en otro archivo la lógica del componente, para que se pueda usar en más de un componente, y para dejar en el componente sólo el renderizado
 import useSearchResults from '../hooks/useSearchResults';
@@ -34,18 +36,20 @@ const SearchScreen = () => {
                 onTermEndEditing={() => searchResults(searchTerm)}  // es decir: llama a esta función cuando ocurre uno de los dos eventos
             />
             <Text>{searchTerm}</Text>
-            <Text>We have found {results.length} results</Text>
             {errorMessge ? <Text>{errorMessge}</Text> : null}
-            <ResultsList title="Cost effective" results={filterResultsByPrice("€")}/>
-            <ResultsList title="Bit pricier" results={filterResultsByPrice("€€")}/>
-            <ResultsList title="Big spender" results={filterResultsByPrice("€€€")}/>
+            {/* Meter los ResultsList dentro de ScrollView para habilitar las barras de desplazamiento vertical en caso de que haya más contenido verticalmente */}
+            <ScrollView> 
+                <ResultsList title="Cost effective" results={filterResultsByPrice("€")} />
+                <ResultsList title="Bit pricier" results={filterResultsByPrice("€€")} />
+                <ResultsList title="Big spender" results={filterResultsByPrice("€€€")} />
+            </ScrollView>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-
+        // marginLeft: 10    // Mejor poner margin left al ResultDetail, sino el contenido se ve un poco cortado!!
     }
 });
 
